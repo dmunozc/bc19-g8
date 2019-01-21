0d6514d4424f2ebd8eed11196bce857d5bee8a91
jlewisbrewer <jbrewer@pdx.edu>
2019-01-21 09:55:19 -0800

    added resource gathering functions

---
+++ bots/blue/resource.js
@@ -0,0 +1,107 @@
+import {BCAbstractRobot, SPECS} from 'battlecode';
+
+// Reads a 2D grid map and returns a list of x, y coordinates for any
+// place on the map that outputs "true"
+export function get_resource_nodes(map) {
+    var list = []
+    for (var i = 0; i < map.length; i++){
+        for (var j = 0; j < map[0].length; j++){
+            if (map[i][j] === true){
+                list.push({x:j, y:i});
+            }
+        }
+    }
+    return list;
+} 
+
+// Returns the item on the list that is closest to the provided
+// location. 
+export function find_nearest_node(loc, list) {
+    var min_dist = 10000000;
+    var index;
+    for (var i = 0; i < list.length; i++) {
+        var dist = (Math.abs(list[i].x - loc.x)) + (Math.abs(list[i].y - loc.y));
+        if (dist < min_dist) {
+            min_dist = dist;
+            index = i;
+        }
+        
+    }
+    return list[index];
+}
+
+// Calculates the distance between two locations
+export function calculate_distance(curr, dest) {
+    var dx = Math.abs(dest.x - curr.x);
+    var dy = Math.abs(dest.y - curr.y);
+    var dist = dx + dy;
+    if (dx === 1 && dy === 1){
+        dist = 1;
+    }
+    return dist;
+}
+
+// Finds the nearest unit of specified type
+export function find_nearest_unit(loc, list, type) {
+    var min_dist = 100000000;
+    var res = {'x':0, 'y':0};
+    var index;
+    for (var i = 0; i < list.length; i++){
+        if (list[i].unit === type) {
+            var dist = (Math.abs(list[i].x - loc.x)) + (Math.abs(list[i].y - loc.y));
+            if (dist < min_dist) {
+                min_dist = dist;
+                index = i;
+            }
+        }
+    }
+    res.x = list[index].x;
+    res.y = list[index].y;
+    return res;
+}
+
+// Updates the map of impassable terrain with current positions
+// export function update_map(loc, map, castle){
+//     var map_loc = map[loc.y][loc.x];
+//     if (castle) {
+//         map[loc.y][loc.x] = "@@@@@@@@@@@@@@@@@@@@@@CASTLE@@@@@@@@@@@@@@@@@@@@";
+//     }
+//     else {
+//         if (map_loc !== false) {
+//             map_loc = false
+//         }
+//     }
+//     // for (var i = 0; i < map.length; i++){
+//     //     for (var j = 0; j< map[0].length; j++){
+//     //         map[i][j] = "CASTLE";
+//     //     }
+//     // }
+//     return map;
+// }
+
+// Returns the dx, dy to from source to a dest coordinate
+export function calculate_move(curr, dest) {
+    // example
+    // source: 35, 35
+    // dest: 24, 36
+    
+    var dx = dest.x - curr.x;
+    var dy = dest.y - curr.y;
+
+    var res = {'x': 0, 'y': 0};
+
+    // We need to move 2 on the x axis
+    if (Math.abs(dx) > 0 && dy === 0) {
+        res.x = (dx/Math.abs(dx)) * 2;
+    }
+    // We need to move 2 on the y axis
+    if (dx === 0 && Math.abs(dy) > 1) {
+        res.y = (dy/Math.abs(dy)) * 2;
+    }
+    // Else we'll move 1 on x and 1 on y;
+    if (Math.abs(dx) > 1 && Math.abs(dy) > 1) {
+        res.x = (dx/Math.abs(dx));
+        res.y = (dy/Math.abs(dy));
+    }
+    return res;
+}
--- bots/blue/robot.js
+++ bots/blue/robot.js
@@ -1,10 +1,53 @@
 import {BCAbstractRobot, SPECS} from 'battlecode';
+import * as resource from './resource.js';
+import pilgrim from './pilgrim.js';
 
 var step = -1;
 
 class MyRobot extends BCAbstractRobot {
+    constructor() {
+        super();
+        this.fuel_cap = 100;
+        this.karb_cap = 20;
+        this.pilgrim_cap;
+        this.num_pilgrims = 0;
+        this.game_map = [];
+        this.karb_locations = [];
+        this.fuel_locations = [];
+
+    
+    }
     turn() {
         step++;
+
+        // This is to initialize our class variables on the first turn
+        if (step === 0) {
+
+            // We need to create a game map that will keep track of all units
+            // and impassable terrain
+            this.game_map = this.getPassableMap();
+            // This section creates a list of karbonite nodes
+            var karb_map = this.getKarboniteMap();
+            this.karb_locations = resource.get_resource_nodes(karb_map);
+
+            //This will create a list of fuel nodes
+            var fuel_map = this.getFuelMap();
+            this.fuel_locations = resource.get_resource_nodes(fuel_map);
+
+            // Creating a pilgrim cap
+            this.pilgrim_cap = 10;
+    
+
+            // if (this.me.unit === SPECS.CASTLE){
+            //     var curr_loc = {'x': this.me.x, 'y':this.me.y};
+            //     // this.castle_locations.push(curr_loc);
+            //     this.log("I am updating map. Curr_loc: " + curr_loc.x + ", " + curr_loc.y);
+            //     this.game_map = resource.update_map(curr_loc, this.game_map, true);
+            //     // this.log(this.game_map);
+
+            // }
+        }
+
 
         if (this.me.unit === SPECS.CRUSADER) {
             // this.log("Crusader health: " + this.me.health);
@@ -13,10 +56,64 @@ class MyRobot extends BCAbstractRobot {
             return this.move(...choice);
         }
 
+        if (this.me.unit === SPECS.PILGRIM) {
+            var curr_loc = {'x': this.me.x, 'y':this.me.y};
+            var visible = this.getVisibleRobots();
+
+            // Find nearest karbonite
+            var nearest_karb = resource.find_nearest_node(curr_loc, this.karb_locations);
+            // Find nearest fuel
+            var nearest_fuel = resource.find_nearest_node(curr_loc, this.fuel_locations);
+            // this.log ("Fuel: " + this.me.fuel + ". Cap: " + this.fuel_cap);
+            // this.log ("Karb: " + this.me.karbonite + ". Cap: " + this.karb_cap);
+            if (this.me.fuel !== this.fuel_cap && this.me.karbonite !== this.karb_cap){
+                if (curr_loc.x === nearest_fuel.x && curr_loc.y === nearest_fuel.y || curr_loc.x === 
+                    nearest_karb.x && curr_loc.y === nearest_karb.y) {
+                        console.log("I am mining!");
+                        this.log("I am carrying " + this.me.fuel + " fuel, and " + this.me.karbonite);
+                        return this.mine();
+                }
+
+                // Moves randomly
+                if (nearest_karb && this.me.fuel === 0 || this.me.karbonite === 0) {
+                    // var move_orders = resource.calculate_move(curr_loc, nearest_fuel);
+                    // // this.log("Move orders: " + move_orders.x + ", " + move_orders.y);
+                    // return this.move(move_orders.x, move_orders.y);
+                    const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+                    const choice = choices[Math.floor(Math.random()*choices.length)]
+                    return this.move(...choice);
+                }
+            }
+            // If we're full we need to return to base
+            else {
+                this.log("I am full! Looking for nearest castle...");
+                var nearest_castle = resource.find_nearest_unit(curr_loc, visible, 0);
+                this.log("Nearest castle is at (" + nearest_castle.x + ", " + nearest_castle.y +")");
+                var dist = resource.calculate_distance(curr_loc, nearest_castle);
+                var move_orders = resource.calculate_move(curr_loc, nearest_castle);
+                this.log("Distance is : " +  dist);
+                if (dist > 1) {
+                    return this.move(move_orders.x, move_orders.y);
+                } else {
+                    this.log("I am unloading resources");
+                    var dx = nearest_castle.x - curr_loc.x;
+                    var dy = nearest_castle.y - curr_loc.y;
+                    return this.give(dx, dy, this.me.karbonite, this.me.fuel);
+                }
+            }
+        }
+
         else if (this.me.unit === SPECS.CASTLE) {
-            if (step % 10 === 0) {
-                this.log("Building a crusader at " + (this.me.x+1) + ", " + (this.me.y+1));
-                return this.buildUnit(SPECS.CRUSADER, 1, 1);
+            // if (step < 10) {
+            //     this.log("Building a pilgrim at " + (this.me.x+1) + ", " + (this.me.y+1));
+            //     return this.buildUnit(pilgrim)
+            // }
+
+            if (step % 10 && this.num_pilgrims < this.pilgrim_cap) {
+                this.log("Building a pilgrim at " + (this.me.x+1) + ", " + (this.me.y+1));
+                this.num_pilgrims++;
+                this.log("There are now: " + this.num_pilgrims + " pilgrims.")
+                return this.buildUnit(SPECS.PILGRIM, 1, 1);
             } else {
                 return // this.log("Castle health: " + this.me.health);
             }
