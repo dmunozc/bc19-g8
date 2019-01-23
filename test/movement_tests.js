97b00404b2faa79c69acbc9afb1cb4ddd2f7c20e
dmunozc <dmunoz11@georgefox.edu>
2019-01-23 13:14:45 -0800

    added unit.js as test unit, added sample test files

---
+++ test/movement_tests.js
@@ -0,0 +1,6 @@
+// load Unit.js module
+var test = require('unit.js');
+var movement = require('bots/red/movement.js');
+var example = 'hello';
+// assert that example variable is a string
+test.string(example);
