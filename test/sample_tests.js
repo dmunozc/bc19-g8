3b7fa3067a98201348e6471c2e24fbe8c756d5d6
unknown <dv5@pdx.edu>
2019-02-26 14:13:24 -0800

    unit tests sample with description

--- test/sample_tests.js
+++ test/sample_tests.js
@@ -5,7 +5,9 @@ describe('Movement', () => {
 describe('get_direction', function(){	
 	//const output = [1,1];
 	it('should return [1,1]', function(){
-		assert.equal(movement.get_direction([0,0],[2,2]), [1,1]);
+		var result = movement.get_direction([0,0],[2,2]);
+		assert.equal(result[0], 1);
+		assert.equal(result[1], 1);
 	});
 });
 });	
