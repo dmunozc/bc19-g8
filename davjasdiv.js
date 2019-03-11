7ca29c4ac1948ebfc9e6beae7b0051ad922c7c86
dmunozc <dmunoz11@georgefox.edu>
2019-03-11 11:53:20 -0700

    final compilation

--- davjasdiv.js
+++ davjasdiv.js
@@ -984,7 +984,7 @@ function find_location_to_build_unit(castleLoc, map, list, resources, r) {
 
     for (var i = 0; i < locations.length; i++){
         var build_loc = {"x": castleLoc.x + locations[i].x, "y": castleLoc.y + locations[i].y};
-        if (build_loc.x > boundary || build_loc.y > boundary){
+        if (build_loc.x >= boundary || build_loc.y >= boundary){
             continue;
         } else if (map[build_loc.y][build_loc.x] === true && find_if_robot_is_present([build_loc.x, build_loc.y], list) === true && find_if_robot_is_present([build_loc.x, build_loc.y], resources) === true){
             //r.log("build loc: " + locations[i].x + ", " + locations[i].y);
